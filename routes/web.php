bad980bb5ff7341fb365ec7c8d32cfaef2669fe5
Glitch12358 <glitch12358@gmail.com>
2024-06-08 08:02:09 +0600

    starting the  view building process

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// <make group hear>
+
+//-----------------------------
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     // Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+//     // Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+//     // Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+//     // Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+//     // Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+//     // Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+//     // Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
+//     Route::resource('note', NoteController::class);
+// });
+
+//--------------------------------
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// </make group hear>
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
